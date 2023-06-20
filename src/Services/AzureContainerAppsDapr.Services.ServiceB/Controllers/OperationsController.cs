3e0b3120256ab37daf2975258b674c9ae51c936b
Mariusz Baran <mariusz.baran23@gmail.com>
2023-06-20 16:41:43 +0200

    Add controllers

---
+++ src/Services/AzureContainerAppsDapr.Services.ServiceB/Controllers/OperationsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureContainerAppsDapr.Services.ServiceB.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OperationsController : ControllerBase
+{
+    [HttpGet("operation-a")]
+    public IActionResult OperationA() => Ok("operation-a");
+
+    [HttpGet("operation-b")]
+    public IActionResult OperationB() => Ok("operation-b");
+    
+    [HttpPost("operation-c")]
+    public IActionResult OperationC() => Ok("operation-c");
+
+    [HttpPost("operation-d")]
+    public IActionResult OperationD() => Ok("operation-d");
+}
--- src/Services/AzureContainerAppsDapr.Services.ServiceB/Program.cs
+++ src/Services/AzureContainerAppsDapr.Services.ServiceB/Program.cs
@@ -2,10 +2,13 @@ using AzureContainerAppsDapr.Shared.Monitoring;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationInsightsMonitoring();
+builder.Services
+    .AddApplicationInsightsMonitoring()
+    .AddControllers();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Service B");
+app.MapControllers();
 
 app.Run();
