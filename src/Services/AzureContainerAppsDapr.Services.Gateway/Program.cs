83f5d1b4ab94a9bf142c18c72be33108bcc1fbc6
Mariusz Baran <mariusz.baran23@gmail.com>
2023-06-20 16:29:35 +0200

    Configure reverse proxy

--- src/Services/AzureContainerAppsDapr.Services.Gateway/Program.cs
+++ src/Services/AzureContainerAppsDapr.Services.Gateway/Program.cs
@@ -2,10 +2,13 @@ using AzureContainerAppsDapr.Shared.Monitoring;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationInsightsMonitoring();
+builder.Services
+    .AddApplicationInsightsMonitoring()
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Gateway");
+app.MapReverseProxy();
 
 app.Run();
