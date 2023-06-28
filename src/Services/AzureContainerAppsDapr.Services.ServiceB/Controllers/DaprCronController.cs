1367878b942cfdc8260233cf990b333cb73a7d9d
Mariusz Baran <mariusz.baran23@gmail.com>
2023-06-28 20:55:41 +0200

    Add cron input binding

---
+++ src/Services/AzureContainerAppsDapr.Services.ServiceB/Controllers/DaprCronController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureContainerAppsDapr.Services.ServiceB.Controllers;
+
+[Authorize(AuthenticationSchemes = "Dapr")]
+[ApiController]
+[Route("dapr/cron")]
+public class DaprCronController : ControllerBase
+{
+    private readonly ILogger<DaprCronController> _logger;
+
+    public DaprCronController(ILogger<DaprCronController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("send-notification")]
+    public IActionResult SendNotification()
+    {
+        _logger.LogInformation("Sending notification: {DateTimeUtc}", DateTime.UtcNow);
+
+        return Ok();
+    }
+}
