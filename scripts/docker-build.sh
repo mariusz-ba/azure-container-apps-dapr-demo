e1d8a1880be3360957efa6d4bc11379f855abeca
Mariusz Baran <mariusz.baran23@gmail.com>
2023-06-29 21:21:56 +0200

    Add docker build scripts

---
+++ scripts/docker-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+image_tag="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
+
+echo "Build docker images: $image_tag"
+
+docker build -f src/Gateway/AzureContainerAppsDapr.Services.Gateway/Dockerfile -t "$ACR_LOGIN_SERVER/gateway:$image_tag" --platform linux/amd64 .
+docker build -f src/Services/AzureContainerAppsDapr.Services.ServiceA/Dockerfile -t "$ACR_LOGIN_SERVER/service-a:$image_tag" --platform linux/amd64 .
+docker build -f src/Services/AzureContainerAppsDapr.Services.ServiceB/Dockerfile -t "$ACR_LOGIN_SERVER/service-b:$image_tag" --platform linux/amd64 .
+
+echo "Docker build complete."
